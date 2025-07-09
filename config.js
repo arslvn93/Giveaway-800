fe757c645cef1b82a1edb0ba5b7beb7f41ecf256
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-07-09 16:57:51 -0400

    Update Site

--- config.js
+++ config.js
@@ -43,8 +43,8 @@ const config = {
         "alt": "Stephen Fabbro real estate sign on a local lawn"
       },
       {
-        "src": "",
-        "alt": "Happy homeowners celebrating their big savings"
+        "src": "https://imgur.com/a/tWrG9Mo",
+        "alt": "SkyGroup x Property.Ca"
       }
     ],
     "includedItems": [
@@ -165,7 +165,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1752094410164_0",
+      "id": "q_auto_1752094670713_0",
       "questionText": "Which of the following home improvements have you considered recently?",
       "options": [
         {
@@ -187,7 +187,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1752094410164_1",
+      "id": "q_auto_1752094670713_1",
       "questionText": "How do you envision your ideal home environment in the next 5 years?",
       "options": [
         {
@@ -209,7 +209,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1752094410164_2",
+      "id": "q_auto_1752094670713_2",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -231,7 +231,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1752094410164_3",
+      "id": "q_auto_1752094670713_3",
       "questionText": "How often do you connect with neighbors and attend local events in Vellore Village?",
       "options": [
         {
