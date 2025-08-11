bee806b9de5ca7256806a87202fae04b9da966d8
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-08-11 11:13:57 -0400

    Update Site

--- config.js
+++ config.js
@@ -141,7 +141,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1754925234726_0",
+      "id": "q_auto_1754925237113_0",
       "questionText": "Which of the following home improvements have you considered recently?",
       "options": [
         {
@@ -163,7 +163,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1754925234726_1",
+      "id": "q_auto_1754925237113_1",
       "questionText": "How do you envision your ideal home environment in the next 5 years?",
       "options": [
         {
@@ -185,7 +185,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1754925234726_2",
+      "id": "q_auto_1754925237113_2",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -207,7 +207,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1754925234726_3",
+      "id": "q_auto_1754925237113_3",
       "questionText": "How often do you connect with neighbors and attend local events in Vellore Village?",
       "options": [
         {
